b3c31eb83ac10145e7f26ed75cd9541c7b758c3f
OverDlive <donghyeok7312@naver.com>
2024-10-29 13:50:01 +0900

    화면 전환 시 지도 표시 고침

--- App/app/src/main/java/com/example/liveguard_app_010/ui/home/HomeFragment.java
+++ App/app/src/main/java/com/example/liveguard_app_010/ui/home/HomeFragment.java
@@ -1,37 +1,27 @@
 package com.example.liveguard_app_010.ui.home;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-
-import com.example.liveguard_app_010.databinding.FragmentHomeBinding;
+import com.example.liveguard_app_010.R;
+import com.naver.maps.map.NaverMapSdk;
 
 public class HomeFragment extends Fragment {
 
-    private FragmentHomeBinding binding;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
-    }
+    private Fragment naverMapFragment;
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return view;
     }
 }
