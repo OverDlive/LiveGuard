c3da32f42efe14ee0d687e0322ede175ec6cea1e
soulplayer01 <d-kim5272@st.kyoto-art.ac.jp>
2024-10-29 16:47:53 +0900

    Bottomsheet 추가

--- App/app/src/main/java/com/example/liveguard_app_010/ui/home/HomeFragment.java
+++ App/app/src/main/java/com/example/liveguard_app_010/ui/home/HomeFragment.java
@@ -1,26 +1,31 @@
 package com.example.liveguard_app_010.ui.home;
 
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
 import com.example.liveguard_app_010.R;
-import com.naver.maps.map.NaverMapSdk;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 public class HomeFragment extends Fragment {
 
-    private Fragment naverMapFragment;
+    private BottomSheetBehavior<View> bottomSheetBehavior;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // Bottom Sheet와 Button 초기화
+        View bottomSheet = view.findViewById(R.id.bottom_sheet);
+
+        // BottomSheetBehavior 설정
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        bottomSheetBehavior.setPeekHeight(300);  // 바텀 시트 초기 높이 설정
+
 
         return view;
     }
