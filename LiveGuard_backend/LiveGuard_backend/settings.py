5ec7e04a8af9445c7f9c6ffcca8e80c3aa95cf18
한동혁 <donghyeok7312@naver.com>
2024-11-15 21:56:49 +0900

    fix settings.py

--- LiveGuard_backend/LiveGuard_backend/settings.py
+++ LiveGuard_backend/LiveGuard_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,6 +55,9 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'LiveGuard_backend.urls'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 CORS_ORIGIN_ALLOW_ALL = True
 
